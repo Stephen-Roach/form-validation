ef860be5ba97530d872d39d2ba9c8ba88129b71d
Stephen-Roach <stephenroach21@gmail.com>
2021-10-16 09:10:52 -0700

    implemented the form validation via js

--- main.js
+++ main.js
@@ -11,6 +11,29 @@ let successIcon = classes('success-icon');
 let failureIcon = classes('failure-icon');
 
 form.addEventListener('submit', (e) => {
-  e.preventDefault;
-  alert('hi');
+  e.preventDefault();
+
+  engine(username, 0, 'Username cannot be blank');
+  engine(email, 1, 'Email cannot be blank');
+  engine(password, 2, 'Password cannot be blank');
 });
+
+// this funciton will handle the form validation for us
+let engine = (id, serial, message) => {
+  if (id.value.trim() === '') {
+    //trim is used to remove the extra white space
+    errorMsg[serial].innerHTML = message;
+    id.style.border = '2px solid red';
+
+    // icons
+    failureIcon[serial].style.opacity = '1';
+    successIcon[serial].style.opacity = '0';
+  } else {
+    errorMsg[serial].innerHTML = '';
+    id.style.border = '2px solid green';
+
+    // icons
+    failureIcon[serial].style.opacity = '0';
+    successIcon[serial].style.opacity = '1';
+  }
+};
